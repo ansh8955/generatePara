478f1ffabe796ef74a3bd8a3ef346133a7de1cfc
Ansh Upadhyay <upadhyayaansh094@gmail.com>
2024-03-11 23:04:36 +0530

    first commit

---
+++ src/screens/Main.js
@@ -0,0 +1,51 @@
+import { Data } from "../Data";
+import { useState } from "react";
+import './Main.css';
+
+const Main = () => {
+  const [updatevalue, setValue] = useState(0);
+  const [text, setText] = useState([]);
+
+  const createpara = (e) => {
+    e.preventDefault();
+
+    let intValue = parseInt(updatevalue);
+
+    if (intValue < 0) {
+      intValue = 1;
+    }
+    if (intValue > 8) {
+      intValue = 8;
+    }
+
+    setText(Data.slice(0, intValue));
+  };
+
+  return (
+    <>
+      <div>
+        <h1>TIRED OF BORING LOREM IPSUM?</h1>
+        <div>
+          <span>Paragraphs</span>
+          <input
+            type="number"
+            value={updatevalue}
+            onChange={(e) => {
+              setValue(parseInt(e.target.value));
+            }}
+          />
+          <button id="genbtn" onClick={createpara}>
+            GENERATE
+          </button>
+        </div>
+
+        <div className="para">
+          {text.map((elem, index) => 
+          <p key={index}>{elem}</p>)}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Main;
